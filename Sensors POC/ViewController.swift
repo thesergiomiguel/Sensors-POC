d3f9b2ac73fd553fef5d640855b3c840d2841d8e
Sérgio Miguel <thesergiomiguel@gmail.com>
2020-08-10 10:08:33 +0200

    Initial Commit

---
+++ Sensors POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sensors POC
+//
+//  Created by Sergio Miguel on 8/10/20.
+//  Copyright © 2020 Kaputt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
