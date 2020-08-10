a8ea6fb27724aa3b3c34ab83e30c16151f28bd31
Sérgio Miguel <thesergiomiguel@gmail.com>
2020-08-10 13:22:19 +0200

    Add a nice green gradient background
    Not something needed for this project, but i wanted to learn how to do it so there's that

--- Sensors POC/MainScreenViewController.swift
+++ Sensors POC/MainScreenViewController.swift
@@ -12,6 +12,34 @@ class MainScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        setup()
+    }
+
+    // MARK: - Setup
+    func setup() {
+        setupBackground()
+    }
+}
+
+// MARK: - Background view
+extension MainScreenViewController {
+    class GradientBackground: UIView {
+        override class var layerClass: AnyClass {
+            return CAGradientLayer.self
+        }
+    }
+
+    func setupBackground() {
+        let backgroundView = GradientBackground(frame: view.bounds)
+
+        if let layer = backgroundView.layer as? CAGradientLayer {
+            layer.colors = [
+                UIColor(red: 0.45, green: 0.75, blue: 0.35, alpha: 1).cgColor,
+                UIColor(red: 0.15, green: 0.75, blue: 0.35, alpha: 1).cgColor,
+                UIColor(red: 0.10, green: 0.80, blue: 0.35, alpha: 1).cgColor,
+            ]
+        }
+
+        view.addSubview(backgroundView)
     }
 }
