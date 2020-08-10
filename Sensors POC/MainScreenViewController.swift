29e4500ff421e9103d110e498fb53784d8b9ea53
Sérgio Miguel <thesergiomiguel@gmail.com>
2020-08-10 10:17:03 +0200

    Add MainScreenViewController and set it as root

---
+++ Sensors POC/MainScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainScreenViewController.swift
+//  Sensors POC
+//
+//  Created by Sergio Miguel on 8/10/20.
+//  Copyright © 2020 Kaputt. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
